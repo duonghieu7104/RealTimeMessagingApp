e0888a855cc009f6dd0306f9d70270634a569341
HieuDuong <duonghieu.7104@gmail.com>
2024-10-16 02:27:38 +0700

    up ui

---
+++ RealTimeMessagingApp/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace RealTimeMessagingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Login_btn = new System.Windows.Forms.Button();
+            this.Resigter_btn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.passw_tb = new System.Windows.Forms.TextBox();
+            this.username_tb = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Search_btn = new System.Windows.Forms.Button();
+            this.list_other_user = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.send_btn = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Login_btn
+            // 
+            this.Login_btn.Location = new System.Drawing.Point(23, 157);
+            this.Login_btn.Name = "Login_btn";
+            this.Login_btn.Size = new System.Drawing.Size(118, 33);
+            this.Login_btn.TabIndex = 0;
+            this.Login_btn.Text = "Đăng nhập";
+            this.Login_btn.UseVisualStyleBackColor = true;
+            // 
+            // Resigter_btn
+            // 
+            this.Resigter_btn.Location = new System.Drawing.Point(224, 157);
+            this.Resigter_btn.Name = "Resigter_btn";
+            this.Resigter_btn.Size = new System.Drawing.Size(118, 33);
+            this.Resigter_btn.TabIndex = 1;
+            this.Resigter_btn.Text = "Đăng kí";
+            this.Resigter_btn.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.passw_tb);
+            this.panel1.Controls.Add(this.username_tb);
+            this.panel1.Controls.Add(this.Login_btn);
+            this.panel1.Controls.Add(this.Resigter_btn);
+            this.panel1.Location = new System.Drawing.Point(12, 383);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(364, 211);
+            this.panel1.TabIndex = 2;
+            // 
+            // passw_tb
+            // 
+            this.passw_tb.Location = new System.Drawing.Point(23, 87);
+            this.passw_tb.Multiline = true;
+            this.passw_tb.Name = "passw_tb";
+            this.passw_tb.Size = new System.Drawing.Size(319, 37);
+            this.passw_tb.TabIndex = 6;
+            // 
+            // username_tb
+            // 
+            this.username_tb.Location = new System.Drawing.Point(23, 27);
+            this.username_tb.Multiline = true;
+            this.username_tb.Name = "username_tb";
+            this.username_tb.Size = new System.Drawing.Size(319, 38);
+            this.username_tb.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.list_other_user);
+            this.panel2.Controls.Add(this.Search_btn);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(364, 365);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textBox2);
+            this.panel3.Controls.Add(this.send_btn);
+            this.panel3.Controls.Add(this.flowLayoutPanel1);
+            this.panel3.Location = new System.Drawing.Point(382, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(791, 582);
+            this.panel3.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Search_btn
+            // 
+            this.Search_btn.Location = new System.Drawing.Point(274, 15);
+            this.Search_btn.Name = "Search_btn";
+            this.Search_btn.Size = new System.Drawing.Size(72, 23);
+            this.Search_btn.TabIndex = 1;
+            this.Search_btn.Text = "Search";
+            this.Search_btn.UseVisualStyleBackColor = true;
+            // 
+            // list_other_user
+            // 
+            this.list_other_user.AutoScroll = true;
+            this.list_other_user.Location = new System.Drawing.Point(16, 53);
+            this.list_other_user.Name = "list_other_user";
+            this.list_other_user.Size = new System.Drawing.Size(326, 293);
+            this.list_other_user.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(14, 16);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(761, 486);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // send_btn
+            // 
+            this.send_btn.Location = new System.Drawing.Point(697, 508);
+            this.send_btn.Name = "send_btn";
+            this.send_btn.Size = new System.Drawing.Size(75, 53);
+            this.send_btn.TabIndex = 6;
+            this.send_btn.Text = "Gửi";
+            this.send_btn.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(14, 508);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(677, 53);
+            this.textBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1185, 606);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Login_btn;
+        private System.Windows.Forms.Button Resigter_btn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox passw_tb;
+        private System.Windows.Forms.TextBox username_tb;
+        private System.Windows.Forms.Button Search_btn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FlowLayoutPanel list_other_user;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button send_btn;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
